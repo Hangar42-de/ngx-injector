bdd83ce6da553fe3c16de16aac80eebac9d26af4
Dennis Rösner <dennis@xn--rsner-jua.de>
2019-09-26 23:44:05 +0200

    Initial Commit

---
+++ projects/ngx-injector/src/lib/injector.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ComponentInjectorService } from './injector.service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [],
+  providers: [ComponentInjectorService]
+})
+export class NgxInjectorModule { }
---
+++ projects/ngx-injector/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-injector
+ */
+
+export * from './lib/injector.service';
+export * from './lib/injector.module';
